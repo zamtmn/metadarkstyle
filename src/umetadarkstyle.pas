18bc581c0a7f33fe9d48604f0a4ea6f1400bda3c
zamtmn <zamtmn@yandex.ru>
2023-03-31 17:35:16 +0500

    first commit

---
+++ src/umetadarkstyle.pas
@@ -0,0 +1,44 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.txt, included in this distribution,                 *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>) 
+}
+
+unit uMetaDarkStyle;
+
+interface
+uses
+  {$IFDEF WINDOWS}
+    {$IF DEFINED(LCLQT5)}
+      uDarkStyle,
+    {$ENDIF}
+    uWin32WidgetSetDark;
+  {$ENDIF}
+
+procedure ApplyMetaDarkStyle;
+procedure MetaDarkFormChanged(Form: TObject);
+
+implementation
+
+procedure ApplyMetaDarkStyle;
+begin
+  {$IFDEF WINDOWS}ApplyDarkStyle;{$ENDIF}
+end;
+
+procedure MetaDarkFormChanged(Form: TObject);
+begin
+  {$IFDEF WINDOWS}DarkFormChanged(Form);{$ENDIF}
+end;
+end.

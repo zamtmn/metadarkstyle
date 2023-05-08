961265dd5356310c5b54b25f0024552539922b63
zamtmn <zamtmn@yandex.ru>
2023-05-08 21:50:40 +0500

    Remove wrong file header

--- src/udarkstyleparams.pas
+++ src/udarkstyleparams.pas
@@ -1,18 +1,4 @@
 {
-*****************************************************************************
-*                                                                           *
-*  This file is part of the ZCAD                                            *
-*                                                                           *
-*  See the file COPYING.txt, included in this distribution,                 *
-*  for details about the copyright.                                         *
-*                                                                           *
-*  This program is distributed in the hope that it will be useful,          *
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
-*                                                                           *
-*****************************************************************************
-}
-{
 @author(Andrey Zubarev <zamtmn@yandex.ru>) 
 }
 

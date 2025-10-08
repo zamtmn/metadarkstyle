d7777f93e34c518e16b6c1640fc858071c397537
zamtmn <zamtmn@yandex.ru>
2025-10-08 13:30:09 +0500

    Change LGPL license to LGPL with exception, as original uwin32widgetsetdar.pas https://github.com/doublecmd/doublecmd/blob/master/src/platform/win/uwin32widgetsetdark.pas

--- src/uwin32widgetsetdark.pas
+++ src/uwin32widgetsetdark.pas
@@ -3,12 +3,24 @@
   -------------------------------------------------------------------------
   Windows dark style widgetset implementation
 
-  Copyright (C) 2021-2023 Alexander Koblov (alexx2000@mail.ru)
+  Copyright (C) 2021-2024 Alexander Koblov (alexx2000@mail.ru)
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+  version 2.1 of the License, or (at your option) any later version
+  with the following modification:
+
+  As a special exception, the copyright holders of this library give you
+  permission to link this library with independent modules to produce an
+  executable, regardless of the license terms of these independent modules,and
+  to copy and distribute the resulting executable under terms of your choice,
+  provided that you also meet, for each linked independent module, the terms
+  and conditions of the license of that module. An independent module is a
+  module which is not derived from or based on this library. If you modify
+  this library, you may extend this exception to your version of the library,
+  but you are not obligated to do so. If you do not wish to do so, delete this
+  exception statement from your version.
 
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
